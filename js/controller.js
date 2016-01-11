c223f53d3fe542596a035a8a9a62f2fcc6d7e7c8
rccoder <rccoder.net@gmail.com>
2016-01-11 21:55:12 +0800

    views and templates

---
+++ js/controller.js
@@ -0,0 +1,15 @@
+/**
+ * Created by rccoder on 2016/1/11.
+ */
+function ViewsAndTemplates($scope) {
+    $scope.peoples = [
+        {
+            name: "rccoder",
+            age: 19
+        },
+        {
+            name: "nickname",
+            age: 20
+        }
+    ];
+};
